6c4ba48ddd0b13a81686c0411293a76705b59d7c
解奕鹏 <3239202719@qq.com>
2018-01-19 19:09:05 +0800

    add 7.3.4

---
+++ Crazy_java/src/Unit7/c7_3_4.java
@@ -0,0 +1,18 @@
+package Unit7;
+
+public class c7_3_4 {
+    public static void main(String[] args) {
+        //弧度转换角度
+        System.out.println("Math toDegrees(1.75):" + Math.toDegrees(1.75));
+        //角度转换弧度
+        System.out.println("Math.toRadians(90):" + Math.toRadians(90));
+        System.out.println(Math.acos(1.2));
+        System.out.println(Math.asin(0.8));
+        System.out.println(Math.atan(2.3));
+        //三角余弦
+        System.out.println(Math.sin(1.0));
+        //双曲余弦
+        System.out.println(Math.sinh(1.0));
+        //...
+    }
+}
