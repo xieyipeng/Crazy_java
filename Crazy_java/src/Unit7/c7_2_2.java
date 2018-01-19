bc31c9473e1645e51cab22990da6ec20bbaf9230
解奕鹏 <3239202719@qq.com>
2018-01-19 11:01:06 +0800

    add 7.2.2

---
+++ Crazy_java/src/Unit7/c7_2_2.java
@@ -0,0 +1,28 @@
+package Unit7;
+
+/**
+ * Runtime类
+ * 运行时环境
+ * 访问JVM的方法
+ */
+public class c7_2_2 {
+    public static void main(String[] args)
+            throws Exception {
+        /**
+         * //获取java程序运行时关联的对象
+         * Runtime runtime=Runtime.getRuntime();
+         * System.out.println("处理数量"+runtime.availableProcessors());
+         * System.out.println("空闲内存数"+runtime.freeMemory());
+         * System.out.println("内存总数"+runtime.totalMemory());
+         * System.out.println("可用最大内存数"+runtime.maxMemory());
+         * //结果：
+         * 处理数量4
+         * 空闲内存数123518560
+         * 内存总数126877696
+         * 可用最大内存数1862270976
+         */
+        Runtime runtime=Runtime.getRuntime();
+        //启动记事本程序
+        runtime.exec("notepad.exe");
+    }
+}
