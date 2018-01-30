9d495371455ca00b7ab42cde63a344fba73d9f14
解奕鹏 <3239202719@qq.com>
2018-01-30 23:23:18 +0800

    add 8.2.1

---
+++ Crazy_java/src/Unit8/c8_2_1.java
@@ -0,0 +1,36 @@
+package Unit8;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * Collection和Iterator接口
+ */
+public class c8_2_1 {
+    public static void main(String[] args) {
+        Collection collection = new ArrayList();
+        collection.add("孙悟空");
+        collection.add(6);
+        System.out.println("collection.size()" + collection.size());
+        collection.remove(6);
+        System.out.println("collection.size()" + collection.size());
+        //判断是否包含“孙悟空”
+        System.out.println("collection.contains孙悟空" + collection.contains("孙悟空"));
+        collection.add("轻量级Java EE企业应用实战");
+        System.out.println("collection" + collection);
+        Collection books = new HashSet();
+        books.add("轻量级Java EE企业应用实战");
+        books.add("Java疯狂讲义");
+        System.out.println("collection是否完全包含books" + collection.containsAll(books));
+        //减去collection里面的books内容
+        collection.removeAll(books);
+        System.out.println("collection" + collection);
+        //删除collection里面的所有元素
+        collection.clear();
+        System.out.println("collection" + collection);
+        //控制books里面只剩下collection包含的元素
+        books.retainAll(collection);
+        System.out.println("books" + books);
+    }
+}
