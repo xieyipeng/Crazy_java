5ef5744dde1c863d03ed6cb7dd162093fe6c9100
解奕鹏 <3239202719@qq.com>
2018-01-30 14:06:16 +0800

    add 7.4.2

--- Crazy_java/src/Unit7/c7_4_2.java
+++ Crazy_java/src/Unit7/c7_4_2.java
@@ -2,7 +2,6 @@ package Unit7;
 
 import java.util.Calendar;
 import java.util.Date;
-
 import static java.util.Calendar.DATE;
 import static java.util.Calendar.MONTH;
 import static java.util.Calendar.YEAR;
@@ -11,6 +10,8 @@ import static java.util.Calendar.YEAR;
  * Calendar类
  */
 public class c7_4_2 {
+    //注意add和roll的区别
+    //Instance 实例
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
         Date date = calendar.getTime();
@@ -25,5 +26,12 @@ public class c7_4_2 {
         System.out.println(calendar1.getTime());
         calendar1.add(MONTH, -8);
         System.out.println(calendar1.getTime());
+
+        //如二月份没有31，会向后面加一；
+        System.out.println("************************");
+        Calendar cal=Calendar.getInstance();
+        cal.set(2003,7,31);
+        cal.set(MONTH,8);
+        System.out.println(cal.getTime());
     }
 }
