67da5414620cfeeacb4c644ce13cb79f9e43d46a
解奕鹏 <3239202719@qq.com>
2018-01-30 23:33:04 +0800

    add 8.2.2

---
+++ Crazy_java/src/Unit8/c8_2_2.java
@@ -0,0 +1,24 @@
+package Unit8;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * lambda±í´ïÊ½
+ */
+public class c8_2_2 {
+    public static void main(String[] args) {
+        Collection books=new HashSet();
+        books.add(1);
+        books.add(2);
+        books.add(3);
+        books.add(4);
+        books.forEach(object -> System.out.println(object));
+//        books.forEach(new Consumer() {
+//            @Override
+//            public void accept(Object object) {
+//                System.out.println(object);
+//            }
+//        });
+    }
+}
