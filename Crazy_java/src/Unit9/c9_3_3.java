efc9c1bc665fb7c979d6832195620711a4de30d5
解奕鹏 <3239202719@qq.com>
2018-02-12 22:28:12 +0800

    add 9.3.3

---
+++ Crazy_java/src/Unit9/c9_3_3.java
@@ -0,0 +1,22 @@
+package Unit9;
+
+/**
+ * 设定类型形参的上限
+ */
+
+class Aplle<T extends Number>{
+
+    T col;
+
+    public static void main(String[] args) {
+        Aplle<Integer> aplle1=new Aplle<>();
+        Aplle<Double> aplle2=new Aplle<>();
+        //下面出错
+        //Aplle<String> aplle3=new Aplle<>();
+    }
+}
+
+//为程序类型形参设定多个上限
+
+public class c9_3_3 <T extends Number & java.io.Serializable{
+}
