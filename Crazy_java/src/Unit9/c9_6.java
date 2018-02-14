86f541f34b7ad16350f90060122e470643929c62
解奕鹏 <3239202719@qq.com>
2018-02-14 17:19:09 +0800

    add 9.6

---
+++ Crazy_java/src/Unit9/c9_6.java
@@ -0,0 +1,13 @@
+package Unit9;
+
+/**
+ * 泛型与数组
+ * 如果一段代码在编译时没有提出未经检查的转换警告，
+ * 那么程序在运行时就不会引发ClassCastException
+ * 异常
+ */
+public class c9_6 {
+    public static void main(String[] args) {
+
+    }
+}
