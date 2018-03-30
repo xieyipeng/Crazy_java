56731c745e2279ad17531b912f28f879ebd5b837
解奕鹏 <32957035+xieyipeng@users.noreply.github.com>
2018-03-30 16:05:43 +0800

    add lanqiaobei-1

---
+++ java/src/ower/test1.java
@@ -0,0 +1,11 @@
+package ower;
+
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String string=scanner.nextLine();
+        System.out.println(string);
+    }
+}
